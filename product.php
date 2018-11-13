69e33adfc7cc9b29b20b96133675e1c07c654231
黃筱婷 <a237666676@gmail.com>
2018-11-13 10:41:05 +0800

    commit pd.php 1113

--- product.php
+++ product.php
@@ -14,58 +14,15 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/animation.gsap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/debug.addIndicators.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/plugins/ScrollToPlugin.min.js"></script>
+    <script src="js/global.js"></script>
 
 
 </head>
 
 <body>
-    <header>
-        <input type="checkbox" name="" id="hb_control">
-        <nav>
-            <svg preserveAspectRatio="none" id="svg-2" viewBox="0 0 100 80" enable-background="new 0 0 100 80">
-                <animate dur="2s" attributeName="x" repeatCount="indefinite" from="0" to="100" xlink:href="#wave"></animate>
-                <defs>
-                    <filter id="f4" x="0" y="0" width="100%" height="100%">
-                        <feOffset result="offOut" in="SourceGraphic" dx="0" dy="0"></feOffset>
-                        <feColorMatrix result="matrixOut" in="offOut" type="matrix" values="0.2 0 0 0 0 0 0.2 0 0 0 0 0 0.2 0 0 0 0 0 1 0"></feColorMatrix>
-                        <feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="2"></feGaussianBlur>
-                        <feBlend in="SourceGraphic" in2="blurOut" mode="normal"></feBlend>
-                    </filter>
-                </defs>
-                <path x="0" y="0" d="M 100,0 L 0,0 L 0,65 Q 12.5,80 25,65T 50,65 Q62.5,80 75,65 T 100,65Z" filter="url(#f4)"
-                    id="wave"></path>
-                <path x="30" y="0" d="M 100,0 L 0,0 L 0,65 Q 12.5,80 25,65T 50,65 Q62.5,80 75,65 T 100,65Z" filter="url(#f4)"
-                    id="wave2"></path>
-            </svg>
-            <div class="ph_nav">
-                <label for="hb_control" class="hb"><span></span></label>
-                <a href="index.html">
-                    <h1 class="phone_logo"><img src="images/logo.png" alt="果然配">果然配</h1>
-                </a>
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li class="menu_item"><a href="diy.html">果汁DIY</a></li>
-                    <li class="menu_item"><a href="product.html">果然特調</a></li>
-                    <li><a href="index.html">
-                            <h1 class="menu_logo"><img src="images/logo.png" alt="果然配">果然配</h1>
-                        </a> </li>
-                    <li class="menu_item"><a href="blog.html">果然私藏</a></li>
-                    <li class="menu_item"><a href="about.html">關於果然</a></li>
-                </ul>
-            </div>
-            <div class="nav_mem">
-                <p><a href="login.html">登入</a></p>
-                <div class="mem_pic"><a href="member.html"><img src="images/user.png" alt="會員中心"></a></div>
-                <div class="mem_pic"><a href="cart.html"><img src="images/cart.png" alt="購物車"></a></div>
-            </div>
-        </nav>
-        <div class="coupon"><img src="images/coupon.png" alt="優惠小遊戲"></div>
-        <div class="robot"><img src="images/robot.png" alt="果然配客服機器人"></div>
-    </header>
-
-
+<?php
+ require_once('nav.php');
+?>
     <section class="pdcate">
         <div id="trigger01"></div>
         <div class="cloud">
@@ -136,10 +93,9 @@
 
 try {
 
-    require_once("connectProducts.php");
+    require_once("connectBooks.php");
     $sql = "select * from offcialpd where offCateNo = 1";
     $products = $pdo -> query( $sql );
-
     while($prodRow = $products->fetchObject()){
 
 ?>
@@ -188,7 +144,6 @@ try {
 
 try {
 
-    require_once("connectProducts.php");
     $sql = "select * from offcialpd where offCateNo = 2";
     $products = $pdo -> query( $sql );
 
@@ -242,7 +197,6 @@ try {
 
 try {
 
-    require_once("connectProducts.php");
     $sql = "select * from offcialpd where offCateNo = 3";
     $products = $pdo -> query( $sql );
 
@@ -292,7 +246,6 @@ try {
 
 try {
 
-    require_once("connectProducts.php");
     $sql = "select * from offcialpd where offCateNo = 4";
     $products = $pdo -> query( $sql );
 
--- productitem.php
+++ productitem.php
@@ -6,57 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>果然配</title>
+    <script src="js/global.js"></script>
     <link rel="stylesheet" href="css/productitem.css">
 </head>
 
 <body>
-
-    <header>
-        <input type="checkbox" name="" id="hb_control">
-        <nav>
-            <svg preserveAspectRatio="none" id="svg-2" viewBox="0 0 100 80" enable-background="new 0 0 100 80">
-                <animate dur="2s" attributeName="x" repeatCount="indefinite" from="0" to="100" xlink:href="#wave"></animate>
-                <defs>
-                    <filter id="f4" x="0" y="0" width="100%" height="100%">
-                        <feOffset result="offOut" in="SourceGraphic" dx="0" dy="0"></feOffset>
-                        <feColorMatrix result="matrixOut" in="offOut" type="matrix" values="0.2 0 0 0 0 0 0.2 0 0 0 0 0 0.2 0 0 0 0 0 1 0"></feColorMatrix>
-                        <feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="2"></feGaussianBlur>
-                        <feBlend in="SourceGraphic" in2="blurOut" mode="normal"></feBlend>
-                    </filter>
-                </defs>
-                <path x="0" y="0" d="M 100,0 L 0,0 L 0,65 Q 12.5,80 25,65T 50,65 Q62.5,80 75,65 T 100,65Z" filter="url(#f4)"
-                    id="wave"></path>
-                <path x="30" y="0" d="M 100,0 L 0,0 L 0,65 Q 12.5,80 25,65T 50,65 Q62.5,80 75,65 T 100,65Z" filter="url(#f4)"
-                    id="wave2"></path>
-            </svg>
-            <div class="ph_nav">
-                <label for="hb_control" class="hb"><span></span></label>
-                <a href="index.html">
-                    <h1 class="phone_logo"><img src="images/logo.png" alt="果然配">果然配</h1>
-                </a>
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li class="menu_item"><a href="diy.html">果汁DIY</a></li>
-                    <li class="menu_item"><a href="product.html">果然特調</a></li>
-                    <li><a href="index.html">
-                            <h1 class="menu_logo"><img src="images/logo.png" alt="果然配">果然配</h1>
-                        </a>
-                    </li>
-                    <li class="menu_item"><a href="blog.html">果然私藏</a></li>
-                    <li class="menu_item"><a href="about.html">關於果然</a></li>
-                </ul>
-            </div>
-            <div class="nav_mem">
-                <p><a href="login.html">登入</a></p>
-                <div class="mem_pic"><a href="member.html"><img src="images/user.png" alt="會員中心"></a></div>
-                <div class="mem_pic"><a href="cart.html"><img src="images/cart.png" alt="購物車"></a></div>
-            </div>
-        </nav>
-        <div class="coupon"><img src="images/coupon.png" alt="優惠小遊戲"></div>
-        <div class="robot"><img src="images/robot.png" alt="果然配客服機器人"></div>
-    </header>
+<?php
+ require_once('nav.php');
+?>
 
     <section class="pdItem">
         <div class="cloud">
@@ -73,7 +30,7 @@
 $offPdNo = $_REQUEST["offPdNo"];
 //連線資料庫
 try{
-  require_once("connectProducts.php");
+  require_once("connectBooks.php");
   $sql = "select * from offcialpd where offPdNo = $offPdNo";
   $products = $pdo->query($sql);
 
